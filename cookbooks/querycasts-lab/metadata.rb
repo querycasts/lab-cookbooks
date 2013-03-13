537bd96e0eae966871405ef6f900e2324476c88a
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-03-12 21:50:33 -0700

    Remove unused files and directories.

--- cookbooks/querycasts-lab/metadata.rb
+++ cookbooks/querycasts-lab/metadata.rb
@@ -3,5 +3,5 @@ maintainer       'Jimmy Cuadra'
 maintainer_email 'jimmy@jimmycuadra.com'
 license          'MIT'
 description      'Provisions the Querycasts Lab'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description 'Provisions the Querycasts Lab'
 version          '0.0.1'
