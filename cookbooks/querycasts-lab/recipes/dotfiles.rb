90262f09f299d70626d57aa772d25d502ac5207c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-03-12 22:38:14 -0700

    Move shell additions into .labrc file.

--- cookbooks/querycasts-lab/recipes/dotfiles.rb
+++ cookbooks/querycasts-lab/recipes/dotfiles.rb
@@ -1,11 +1,25 @@
-cookbook_file "/home/vagrant/.bashrc" do
-  source "bashrc"
-  owner "vagrant"
-  group "vagrant"
-end
-
 cookbook_file "/home/vagrant/.gemrc" do
   source "gemrc"
   owner "vagrant"
   group "vagrant"
+end
+
+cookbook_file "/home/vagrant/.labrc" do
+  source "labrc"
+  owner "vagrant"
+  group "vagrant"
+end
+
+execute "load labrc from bashrc" do
+  command <<-CMD
+cat <<SCRIPT >>/home/vagrant/.bashrc
+# Load Querycasts Lab Bash additions
+if [ -f ~/.labrc ]; then
+  source ~/.labrc
+fi
+SCRIPT
+CMD
+  user "vagrant"
+  group "vagrant"
+  not_if { File.read("/home/vagrant/.bashrc").include?("Querycasts") }
 end
